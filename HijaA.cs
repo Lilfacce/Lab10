0e3fc4f644028fe7162c6c7a1d15ec1bdf2e24da
Frida Bolaños <fbolanos@cognits.co>
2024-04-25 09:22:25 -0600

    Agregar archivos de proyecto.

---
+++ HijaA.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab10
+{
+    public class HijaA:Padre
+    {
+       public string OtraCadena {  get; set; }
+        public HijaA()
+        {
+            OtraCadena = string.Empty;
+        }
+    }
+}
---
+++ Profesor.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab10
+{
+    public class Padre
+    {
+        public int Id { get; set; }
+        public string Cadena { get; set; }
+        public DateTime Fecha { get; set; }
+
+        //Construxtor: se ejecuta cada vez que se crea
+
+        private string DatoPrivado { get; set; }
+
+        protected string DatoProtegido { get; set; }
+
+        public Padre()
+        {
+            Cadena = "";
+            Fecha = DateTime.Now;
+        }
+    }
+}
